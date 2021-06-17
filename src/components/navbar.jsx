03734587b8037811427c2d822f9f36c1989ba567
yoonsery <yoon.sery@icloud.com>
2021-06-17 16:16:29 +0900

    change font color

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = memo((props) => {
   return (
     <nav className="navbar">
       <i className="navbar-logo fab fa-pagelines"></i>
-      <span>Habit Tracker</span>
+      <span className="navbar-title">Habit Tracker</span>
       <span className="navbar-count">{props.totalCount}</span>
       <button className="habits-reset" onClick={props.onReset}>
         Reset All
